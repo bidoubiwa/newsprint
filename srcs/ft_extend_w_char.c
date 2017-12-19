c53a7f5cc5250376daead01be38678dcb3821306
Charlotte VERMANDEL <cvermand@e1r3p4.42.fr>
2017-12-19 14:42:43 +0100

    aaa

--- srcs/ft_extend_w_char.c
+++ srcs/ft_extend_w_char.c
@@ -6,11 +6,22 @@
 /*   By: cvermand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/18 20:57:42 by cvermand          #+#    #+#             */
-/*   Updated: 2017/12/18 22:22:10 by cvermand         ###   ########.fr       */
+/*   Updated: 2017/12/19 13:09:05 by cvermand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "hprintf.h"
+
+int			ft_number_byte_for_carac(char *show)
+{
+	int byte;
+	int y;
+
+	y = 0;
+	while (elem->show[y] == '1')
+		y++;
+	return (byte);
+}
 
 char		*ft_prec_on_w(t_chain *elem)
 {
@@ -23,18 +34,29 @@ char		*ft_prec_on_w(t_chain *elem)
 	carac = 0;
 	if (!(elem->prec < elem->nbr_byte))
 		return (elem->show);
-	return (elem->show);
+	
+	while (carac < elem->prec)
+	{
+		if (elem->show[carac] == '0')
+			byte++;
+		else if 
+		{
+			while 
+		}
+	}
+	
 	while (i < elem->prec)
 	{
-		while (elem->show[carac++] == '1')
+		while (elem->show[carac] && elem->show[carac++] == '1')
 			byte++;
 		if (i + byte <= elem->prec)
 			i = i + byte;
+		else if (elem->show[carac] &&  == '0')
 		else
 			break ;
 		byte = 0;
 		carac = i * 8;
 	}
-	//new = ft_strnew()
+	new = ft_strnew()
 	return (elem->show);
 }
